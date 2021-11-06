6395a625e4134e38d1ce19500b7669e1d249af1c
Jesse Tran <jessetran03@gmail.com>
2021-11-06 17:33:03 -0500

    update folder import

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import registerIcons from "./utilities/registerIcons";
+import registerIcons from "./utils/registerIcons";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
