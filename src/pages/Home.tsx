974a48645c9d57834a92550b7fd3ac8c070a969d
Jesse Tran <jessetran03@gmail.com>
2021-11-18 22:08:38 -0600

    Update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
 const Home = () => {
-  return <div className="">Aim to be Fit</div>;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="text-5xl mt-10 font-semibold">
+        Aim to be Fit
+      </div>
+      <div className="text-xl mt-5">Begin your journey.</div>
+    </div>
+  );
 };
 
 export default Home;
